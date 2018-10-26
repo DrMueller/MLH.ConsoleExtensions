665839a28dd210f6ae6003a13fcca9e7c2a39f0d
Matthias Mueller <matthiasm@live.de>
2018-10-26 20:58:37 +0200

    WIP

--- Sources/Application/Areas/Commands/Services/Implementation/ConsoleCommandsContainer.cs
+++ Sources/Application/Areas/Commands/Services/Implementation/ConsoleCommandsContainer.cs
@@ -21,7 +21,9 @@ namespace Mmu.Mlh.ConsoleExtensions.Areas.Commands.Services.Implementation
 
         public void ShowCommands()
         {
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
             ListenForInputs();
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
         }
 
         private void DisplayCommands()
