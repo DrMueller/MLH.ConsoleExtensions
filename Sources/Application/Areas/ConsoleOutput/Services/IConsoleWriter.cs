c67bb9600d957889c39be917a003645a9dcb8928
Matthias Mueller <matthiasm@live.de>
2019-03-25 20:28:03 +0100

    WIP

--- Sources/Application/Areas/ConsoleOutput/Services/IConsoleWriter.cs
+++ Sources/Application/Areas/ConsoleOutput/Services/IConsoleWriter.cs
@@ -4,8 +4,8 @@ namespace Mmu.Mlh.ConsoleExtensions.Areas.ConsoleOutput.Services
 {
     public interface IConsoleWriter
     {
-        void Write(string text, ConsoleColor? backgroundColor, ConsoleColor? foregroundColor);
+        void Write(string text, ConsoleColor? backgroundColor = ConsoleColor.Black, ConsoleColor? foregroundColor = ConsoleColor.Gray);
 
-        void WriteLine(string text, ConsoleColor? backgroundColor, ConsoleColor? foregroundColor);
+        void WriteLine(string text, ConsoleColor? backgroundColor = ConsoleColor.Black, ConsoleColor? foregroundColor = ConsoleColor.Gray);
     }
 }
